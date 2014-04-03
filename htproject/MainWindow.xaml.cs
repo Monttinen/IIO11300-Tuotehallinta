c430cf260ea6b3036a96d1aecde0ce767688d667
Monttinen <minkkinen7@hotmail.com>
2014-04-03 09:30:01 +0300

    xaml napeille jne nimet, lisätty paketti events

--- htproject/MainWindow.xaml.cs
+++ htproject/MainWindow.xaml.cs
@@ -43,7 +43,7 @@ namespace ProductManagement
 
     }
 
-    private void Button_Click(object sender, RoutedEventArgs e)
+    private void btnConnect_Click(object sender, RoutedEventArgs e)
     {
       if (!Connected)
         Connect();
@@ -82,49 +82,12 @@ namespace ProductManagement
       
     }
 
-    private void getDummyData()
-    {
-      // lisätään testaamista varten kamaa listoihin
-      Product product1 = new Product(0, "Aamupala", "Aamupala yhdelle", 10.0);
-      Products.Add(product1);
-      Product product2 = new Product(1, "Lounas", "Lounas yhdelle", 15.0);
-      Products.Add(product2);
-      Product product3 = new Product(2, "Illallinen", "Illallinen yhdelle", 20.0);
-      Products.Add(product3);
-      Product product4 = new Product(3, "Yömyssy", "you know", 10.0);
-      Products.Add(product4);
-
-      Product product5 = new Product(4, "Liinavaate", "Liinavaatteet huoneeseen", 5.0);
-      Products.Add(product5);
-
-      Category tmp = new Category(0, "Ruokailu", "Sisältää ruokailut");
-      tmp.AddProduct(product1);
-      tmp.AddProduct(product2);
-      tmp.AddProduct(product3);
-      tmp.AddProduct(product4);
-      Categories.Add(tmp);
-
-      tmp = new Category(1, "Huoneen varattavat", "Sisältää huoneeseen varattavia tuotteita");
-      tmp.AddProduct(product5);
-      Categories.Add(tmp);
-
-      Categories.Add(new Category(2, "Joku kategoria", "Sisältää tietoa"));
-      Categories.Add(new Category(3, "Vielä yksi kategoria", "Sisältää myös tietoa"));
-
-      Package package1 = new Package(0, "Yöpyminen", 120.0, 123);
-      package1.AddProduct(product1);
-      package1.AddProduct(product5);
-
-      Packages.Add(package1);
-
-    }
-
     private bool checkLogin(string user, string password)
     {
       try
       {
         var result = from u in db.users
-                     where u.password == password
+                     where u.password == password && u.username == user
                      select u;
         if (result.Count() > 0)
         {
@@ -143,5 +106,7 @@ namespace ProductManagement
       }
         
     }
+
+
   }
 }
